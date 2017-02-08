0a301c242b03e3dc549150669781d5784da9f5ef
priya <jkster@gmail.com>
2017-02-07 21:59:33 -0500

    add validate age add validate name add error messages for wrong input in age/name fields

--- app/src/main/java/com/example/priya/intent_demo_lec6/MainActivity.java
+++ app/src/main/java/com/example/priya/intent_demo_lec6/MainActivity.java
@@ -12,12 +12,14 @@ import android.widget.ListView;
 
 import static com.example.priya.intent_demo_lec6.Utils.Utils.AGE_MSG_KEY;
 import static com.example.priya.intent_demo_lec6.Utils.Utils.NAME_MSG_KEY;
+import static java.lang.Integer.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn1;
     private EditText edTxt1, edTxt2;
     private ListView lsView;
+    private String age, name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         lsView = (ListView)findViewById(R.id.ls_view1_ma1);
         final String[] myItems = getResources().getStringArray(R.array.my_itm_lst);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myItems);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myItems);
         lsView.setAdapter(adapter);
 
         lsView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
@@ -40,14 +42,23 @@ public class MainActivity extends AppCompatActivity {
         });
         edTxt1 = (EditText)findViewById(R.id.edtTxt_ma1);
         edTxt2 = (EditText)findViewById(R.id.edtTxt2_ma1);
+
         btn1 = (Button)findViewById(R.id.btn_ma1);
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(MainActivity.this, Main2Activity.class);
-                intent2.putExtra(AGE_MSG_KEY, "Age is:  " + Integer.parseInt(edTxt1.getText().toString()));
-                intent2.putExtra(NAME_MSG_KEY, "Name is: " + edTxt2.getText().toString());
-                startActivity(intent2);
+                age =  edTxt1.getText().toString();
+                name =  edTxt2.getText().toString();
+                if((age.length()==0)||(name.length()==0)){
+                    if(age.length()==0){edTxt1.setError("Age cannot be empty");}
+                    else {edTxt2.setError("Name cannot be empty");}
+                }
+                else{
+                    Intent intent2 = new Intent(MainActivity.this, Main2Activity.class);
+                    intent2.putExtra(AGE_MSG_KEY, "Age is:  " + Integer.parseInt(age));
+                    intent2.putExtra(NAME_MSG_KEY, "Name is: " + name);
+                    startActivity(intent2);
+                }
             }
         });
 
